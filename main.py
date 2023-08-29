90cc544bb22e371052a2f006cb226be13a0c96f3
YasinAflatoon <Yasinaflatoonzade@gmail.com>
2023-08-29 15:13:09 +0330

    Handled some exceptions and added json database.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import json
 
 
 # ------- PASSWORD GENERATOR ------- #
@@ -39,12 +40,65 @@ def save():
         user_confirm = messagebox.askokcancel(title=webpage, message=f"Username: {username}\nPassword: {password}\n"
                                                                      f"Do You Want to Save This Login Info?")
         if user_confirm:
-            with open("saved-passwords.txt", mode="a") as datafile:
-                datafile.write(f"{webpage} | {username} | {password}\n")
+            new_info = {
+                webpage: {
+                    "username": username,
+                    "password": password}
+            }
+            try:
+                datafile = open("saved-passwords.json", mode="r")
+
+            except FileNotFoundError:
+                datafile = open("saved-passwords.json", mode="w")
+                datafile.close()
+            else:
+                datafile.close()
+
+            try:
+                with open("saved-passwords.json", "r") as datafile:
+                    file_content = json.load(datafile)
+
+            except json.decoder.JSONDecodeError:
+                with open("saved-passwords.json", "a") as datafile:
+                    json.dump(new_info, datafile, indent=4)
+            else:
+                file_content.update(new_info)
+                with open("saved-passwords.json", mode="w") as datafile:
+                    json.dump(file_content, datafile, indent=4)
+
             messagebox.showinfo(title="Success!", message="Successfully Added!")
             web_entry.delete(0, END)
             pass_entry.delete(0, END)
             web_entry.focus()
+
+
+def search():
+    webpage = (web_entry.get()).strip()
+
+    try:
+        with open("saved-passwords.json", "r") as datafile:
+            file_data = json.load(datafile)
+
+    except FileNotFoundError:
+        messagebox.showerror(title="Error!", message="Failed to detect a data file!\n"
+                                                     "Please make sure you have added a login info to the app.")
+    except json.decoder.JSONDecodeError:
+        messagebox.showwarning(title="Not Found!", message=f"No match found for \"{webpage}\"!\n"
+                                                           f"Check for any possible typos if you are sure you have "
+                                                           f"added this login info.")
+    else:
+        item_found = False
+        for key in file_data:
+            if key.lower() == webpage.lower():
+                messagebox.showinfo(title=key, message=f"Username: {file_data[key]['username']}\n"
+                                                       f"Password: {file_data[key]['password']}")
+                item_found = True
+                break
+
+        if not item_found:
+            messagebox.showwarning(title="Not Found!", message=f"No login data found for \"{webpage}\"!\n"
+                                                               f"Check for any possible typos if you are sure you have "
+                                                               f"added this login info.")
 
 
 # ------- UI SETUP ------- #
@@ -75,13 +129,16 @@ web_entry.focus()
 user_entry = Entry(width=35)
 user_entry.grid(row=2, column=1, columnspan=2)
 
-pass_entry = Entry(width=17)
+pass_entry = Entry(width=35)
 pass_entry.grid(row=3, column=1)
 
 pass_gen = Button(text="Generate Password", command=generate_pass)
-pass_gen.grid(row=3, column=2)
+pass_gen.grid(row=3, column=3)
 
 add_button = Button(text="Add", width=30, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+
+search_button = Button(text="Search", command=search)
+search_button.grid(row=1, column=3)
 
 window.mainloop()
